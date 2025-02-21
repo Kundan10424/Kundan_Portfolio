a85539748e1e5ac280c8704285eea54362c06488
Kundan10424 <kundankumar10424>
2025-02-21 14:12:39 +0530

    modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,9 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link to="/" className="flex items-center gap-2" onClick={() => setActive("")}>
+      <Link to="/" className="flex items-center gap-2" onClick={() => {setActive(""); window.scrollTo({ top: 0, behavior: "smooth" });
+  }}
+>
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Kundan &nbsp;<span className="sm:block hidden">| Kumar</span>
